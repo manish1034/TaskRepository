e4bfa4064343c8c4bf397d7193da462d67abafe9
manish1034 <76244041+manish1034@users.noreply.github.com>
2024-05-08 16:51:40 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center; 
+  align-items: center; 
+  height: 100vh; 
+}
+
+.img-size {
+  width: 100px;
+  height: auto;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="home">
+        <h1>Manish Testing....</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+    
