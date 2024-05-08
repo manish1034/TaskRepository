a59967761acbe47e7718475ed5ff119f9b7a9930
manish1034 <76244041+manish1034@users.noreply.github.com>
2024-05-08 18:32:43 +0530

    Added checkbox for changing color of text to red.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,105 @@
 <template>
     <div id="home">
-        <h1>Manish Testing....</h1>
+        <h1 :class="{red: isRed}">{{message}} Testing....</h1>
+        {{date}}
+        <div class="input-field">
+            <input type="text" v-model="message">
+            <button @click="clearMessage">clear</button>
+            <button @click="capsMessage">caps</button>
+            <input class="check-input" :checked="isRed" v-model="isRed" type="checkbox" @click="red" /> <span>Red</span>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data(){
+    return{
+        message:"Manish",
+        date: false,
+        isRed: false,
+    }
+  },
+  created: function() {
+    this.date =
+      new Date().getFullYear() +
+      "-" +
+      new Date().getMonth() +
+      "-" +
+      new Date().getDay();
+  },
+  mounted: function() {
+    this.date =
+      new Date().getFullYear() +
+      "-" +
+      new Date().getMonth() +
+      "-" +
+      new Date().getDay();
+  },
+  methods: {
+    clearMessage() {
+      this.message = ""; // Clear the message text
+    },
+    capsMessage() {
+        this.message = this.message.toUpperCase();
+    },
+    red: function() {
+        // eslint-disable-next-line
+        console.log(this.isRed)
+    }
+  },
 }
 </script>
     
+<style>
+h1 {
+    text-align: center;
+}
+
+.input-field {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+}
+
+input {
+    margin-top: 1rem;
+    border: 1px solid black;
+    border-radius: 4px;
+    padding: 8px;
+}
+
+input:focus {
+    border: none;
+    outline: 2px solid blue;
+}
+
+button {
+    border: 1px solid black;
+    margin-top: 1rem;
+    padding: 4px 8px;
+    height: 2.6rem;
+    border-radius: 4px;
+    align-content: center;
+}
+
+button:hover {
+    background: blanchedalmond;
+    color: gray;
+}
+
+span {
+    margin-top: 1.5rem;
+    padding-left: 0.5rem;
+}
+
+.red {
+  color: red;
+}
+
+.green {
+  color: green;
+}
+
+</style>
