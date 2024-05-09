32b922a7477a90748766bb7e9791e353c164b258
manish1034 <76244041+manish1034@users.noreply.github.com>
2024-05-09 16:07:02 +0530

    Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,34 @@
+<template>
+    <button id="btn" @click="handleClick">
+      Button {{ name }}
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    name: "Button",
+    props: {
+      name: String
+    },
+    methods: {
+      handleClick() {
+        this.$emit("button-click", this.name); // Emit event with button name
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  #btn {
+    background: #007bff;
+    padding: 1rem;
+    margin: 15px;
+    color: aliceblue;
+    border-radius: 4px;
+  }
+  
+  #btn:hover {
+    background: #0458b1;
+  }
+  </style>
+  
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,46 @@
 <template>
-    <div id="home">
-        <h1>Manish Testing....</h1>
-    </div>
+  <div id="home">
+    <Button name="1" @button-click="showAlert('Button 1 clicked!')" />
+    <Button name="2" @button-click="toggleMessage" />
+    <p>{{ msg }}</p>
+  </div>
 </template>
 
 <script>
+import Button from './Button.vue'
+
 export default {
-  name: 'home'
-}
+  name: 'Home',
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      msg: "Yosh!!!",
+      isMsgVisible: true
+    };
+  },
+  methods: {
+    showAlert(message) {
+      alert(message);
+    },
+    toggleMessage() {
+      this.isMsgVisible = !this.isMsgVisible;
+      this.msg = this.isMsgVisible ? "Yosh!!!" : "Message toggled!";
+    }
+  }
+};
 </script>
-    
+
+<style>
+#home {
+  max-width: 600px;
+  margin: auto;
+  padding: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+p {
+  text-align: center;
+}
+</style>
