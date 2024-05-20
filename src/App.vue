5cac7014d13f7aa528c8b2076be76525ec0aea53
manish1034 <76244041+manish1034@users.noreply.github.com>
2024-05-20 12:59:02 +0530

    made a user interface having signup and login with logout functionality using firebase and vuex

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,48 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <Home />
+  <TheNavigation />
+  <the-loader v-if="showLoading"></the-loader>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div>
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Home from './components/Home.vue';
+import TheNavigation from "./components/TheNavigation.vue";
+import { mapState } from "vuex";
+import { AUTO_LOGIN_ACTION } from "./store/storeconstants";
+import { defineAsyncComponent } from "vue";
 
+const TheLoader = defineAsyncComponent(() =>
+  import(/* webpackChunkName: "Loader" */ "./components/TheLoader.vue")
+);
 export default {
-  name: 'app',
+  name: "App",
+  computed: {
+    ...mapState({
+      showLoading: (state) => state.showLoading,
+      autoLogout: (state) => state.auth.autoLogout,
+    }),
+  },
+
+  watch: {
+    autoLogout(curValue, oldValue) {
+      if (curValue && curValue != oldValue) {
+        this.$router.replace("/login");
+      }
+    },
+  },
   components: {
-    Home
-  }
-}
+    TheNavigation,
+    TheLoader,
+  },
+  created() {
+    this.$store.dispatch(`auth/${AUTO_LOGIN_ACTION}`);
+  },
+};
 </script>
-
-<style>
-#app {
-  display: flex;
-  flex-direction: column;
-  justify-content: center; 
-  align-items: center; 
-  height: 100vh; 
-}
-
-.img-size {
-  width: 100px;
-  height: auto;
-}
-</style>
